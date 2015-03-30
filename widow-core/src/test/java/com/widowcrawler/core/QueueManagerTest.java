2c7e447db6568c7bea61c1c6070cc5657cb650b5
Scott Mansfield <sgmansf@gmail.com>
2015-03-30 01:34:58 -0700

    Updating dependencies for powermock, updating a test

--- widow-core/src/test/java/com/widowcrawler/core/QueueManagerTest.java
+++ widow-core/src/test/java/com/widowcrawler/core/QueueManagerTest.java
@@ -1,14 +1,24 @@
 package com.widowcrawler.core;
 
-import static org.junit.Assert.*;
-
+import com.amazonaws.services.sqs.AmazonSQSAsyncClient;
+import com.amazonaws.services.sqs.model.GetQueueUrlResult;
 import com.widowcrawler.core.queue.QueueManager;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.easymock.EasyMock.expect;
+import static org.junit.Assert.assertNull;
+import static org.powermock.api.easymock.PowerMock.*;
 
 /**
  * @author Scott Mansfield
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({System.class, QueueManager.class, QueueManagerTest.class})
 public class QueueManagerTest {
 
     private QueueManager queueManager;
@@ -19,17 +29,33 @@ public class QueueManagerTest {
     }
 
     @Test
-    public void constructor_queuePropertyExists_messagesMapCreated() {
+    public void constructor_queuePropertyExistsAndQueuesExist_messagesMapCreated() throws Exception {
 
-        // ARRAGE
-        System.setProperty("com.widowcrawler.queues", "foo           | bar |baz|||||");
+        //// ARRANGE
+        mockStatic(System.class);
+        expect(System.getProperty("com.widowcrawler.queues")).andReturn("foo           | bar |baz|||||").once();
 
-        // ACT
-        this.queueManager = new QueueManager();
+        AmazonSQSAsyncClient sqsAsyncClient = createMock(AmazonSQSAsyncClient.class);
 
-        // ASSERT
+        for (String queueName : new String[] { "foo", "bar", "baz"}) {
+            expect(sqsAsyncClient.getQueueUrl(queueName))
+                    .andReturn(new GetQueueUrlResult().withQueueUrl(queueName + ".queueUrl")).once();
+        }
+
+        FieldUtils.writeField(this.queueManager, "sqsClient", sqsAsyncClient, true);
+
+        replay(System.class);
+        replay(sqsAsyncClient);
+
+        //// ACT
+        this.queueManager.postConstruct();
+
+        //// ASSERT
         assertNull(this.queueManager.nextMessage("foo"));
         assertNull(this.queueManager.nextMessage("bar"));
         assertNull(this.queueManager.nextMessage("baz"));
+
+        verify(System.class);
+        verify(sqsAsyncClient);
     }
 }
