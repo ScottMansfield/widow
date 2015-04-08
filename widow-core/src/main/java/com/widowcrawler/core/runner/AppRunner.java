fda1246f6114283a8307e54eeb0df658fd916481
Scott Mansfield <sgmansf@gmail.com>
2015-04-08 00:00:07 -0700

    Consolidating app running logic into one reusable class

--- widow-core/src/main/java/com/widowcrawler/core/runner/AppRunner.java
+++ widow-core/src/main/java/com/widowcrawler/core/runner/AppRunner.java
@@ -1,7 +1,38 @@
 package com.widowcrawler.core.runner;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+import com.netflix.governator.guice.LifecycleInjector;
+import com.netflix.governator.lifecycle.LifecycleManager;
+import com.widowcrawler.core.dispatch.Dispatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Created by smansfield on 4/7/15.
+ * @author Scott Mansfield
  */
 public class AppRunner {
+
+    private static Logger logger = LoggerFactory.getLogger(AppRunner.class);
+
+    public static void run(Class<? extends AbstractModule> moduleClass) throws Exception {
+        Injector injector = LifecycleInjector.builder()
+                .withModuleClass(moduleClass)
+                .usingBasePackages("com.widowcrawler")
+                .build()
+                .createInjector();
+
+        LifecycleManager lifecycleManager = injector.getInstance(LifecycleManager.class);
+        lifecycleManager.start();
+
+        Dispatcher dispatcher = injector.getInstance(Dispatcher.class);
+        boolean cont = true;
+
+        while (cont) {
+            logger.info("Dispatching...");
+            cont = dispatcher.dispatch();
+        }
+
+        lifecycleManager.close();
+    }
 }
