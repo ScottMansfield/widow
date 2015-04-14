ecabae25baf0985ee54e39e76dc67991a6a5e909
Scott Mansfield <sgmansf@gmail.com>
2015-04-14 00:05:56 -0700

    PageAttribute contains metadata about the type and has extra types

--- widow-core/src/main/java/com/widowcrawler/core/model/PageAttribute.java
+++ widow-core/src/main/java/com/widowcrawler/core/model/PageAttribute.java
@@ -4,15 +4,38 @@ package com.widowcrawler.core.model;
  * @author Scott Mansfield
  */
 public enum PageAttribute {
+    ORIGINAL_URL,
+    STATUS_CODE(Type.NUMBER),
+    HEADERS,
+    PAGE_CONTENT_REF,
     LOCALE,
-    TIME_ACCESSED,
-    LOAD_TIME_MILLIS,
-    RESPONSE_SIZE,
+    TIME_ACCESSED(Type.NUMBER),
+    LOAD_TIME_MILLIS(Type.NUMBER),
+    RESPONSE_SIZE(Type.NUMBER),
     TITLE,
     OUT_LINKS,
     CSS_LINKS,
     JS_LINKS,
     IMG_LINKS,
-    CONTENT_SIZE,
-    SIZE_WITH_ASSETS
+    CONTENT_SIZE(Type.NUMBER),
+    SIZE_WITH_ASSETS(Type.NUMBER);
+
+    public static enum Type {
+        STRING,
+        NUMBER
+    }
+
+    private Type type;
+
+    private PageAttribute() {
+        this.type = Type.STRING;
+    }
+
+    private PageAttribute(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
 }
