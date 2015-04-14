6530e37ae8f37e4b683e00d543effb2ddfed893d
Scott Mansfield <sgmansf@gmail.com>
2015-04-14 00:12:03 -0700

    Indexing to a DynamoDB table now

--- widow-index/src/main/java/com/widowcrawler/index/IndexWorker.java
+++ widow-index/src/main/java/com/widowcrawler/index/IndexWorker.java
@@ -1,9 +1,21 @@
 package com.widowcrawler.index;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import com.amazonaws.services.dynamodbv2.model.ReturnConsumedCapacity;
 import com.widowcrawler.core.model.IndexInput;
+import com.widowcrawler.core.model.PageAttribute;
+import com.widowcrawler.core.retry.Retry;
 import com.widowcrawler.core.worker.Worker;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Scott Mansfield
@@ -11,6 +23,15 @@ import org.slf4j.LoggerFactory;
 public class IndexWorker extends Worker {
 
     private static final Logger logger = LoggerFactory.getLogger(IndexWorker.class);
+
+    // TODO: I really ought to get working on a config system for this...
+    private static final String TABLE_NAME = "widow-test";
+
+    @Inject
+    AmazonDynamoDB dynamoDBClient;
+
+    @Inject
+    Retry retry;
 
     private IndexInput indexInput;
 
@@ -21,8 +42,52 @@ public class IndexWorker extends Worker {
 
     @Override
     protected boolean doWork() {
-        logger.info("Received IndexInput:" + indexInput.getOriginalURL());
+        try {
+            logger.info("Received IndexInput: " + indexInput.getAttribute(PageAttribute.ORIGINAL_URL));
 
-        return true;
+            // Check key fields to make sure they exist
+            if (!indexInput.getAttributes().keySet().contains(PageAttribute.ORIGINAL_URL) ||
+                    !indexInput.getAttributes().keySet().contains(PageAttribute.TIME_ACCESSED)) {
+                throw new IllegalArgumentException("Attributes ORIGINAL_URL and TIME_ACCESSED must exist");
+            }
+
+            Map<String, AttributeValue> attributeValueMap = indexInput.getAttributes().entrySet()
+                    .stream()
+                    .filter((entry) -> entry.getValue() != null)
+                    .filter((entry) -> StringUtils.isNotBlank(entry.getValue().toString()))
+                    .collect(Collectors.toMap(
+                            e -> e.getKey().toString(),
+                            e -> {
+                                switch (e.getKey().getType()) {
+                                    case NUMBER:
+                                        return new AttributeValue().withN(e.getValue().toString());
+
+                                    case STRING:
+                                    default:
+                                        return new AttributeValue().withS(e.getValue().toString());
+                                }
+                            }
+                    ));
+
+            // TODO: Can we include type information for the attributes?
+            // and hack it up I guess?
+            attributeValueMap.put(PageAttribute.TIME_ACCESSED.toString(),
+                    new AttributeValue().withN(indexInput.getAttribute(PageAttribute.TIME_ACCESSED).toString()));
+
+            PutItemRequest putItemRequest = new PutItemRequest()
+                    .withTableName(TABLE_NAME)
+                    .withItem(attributeValueMap)
+                    .withReturnConsumedCapacity(ReturnConsumedCapacity.TOTAL);
+
+            final PutItemResult putItemResult = retry.retry(() -> dynamoDBClient.putItem(putItemRequest));
+
+            logger.info("Consumed table capacity: " + putItemResult.getConsumedCapacity().getCapacityUnits());
+
+            return true;
+
+        } catch (Exception ex) {
+            logger.error("Error while indexing.", ex);
+            return false;
+        }
     }
 }
--- widow-index/src/main/java/com/widowcrawler/index/module/WidowIndexModule.java
+++ widow-index/src/main/java/com/widowcrawler/index/module/WidowIndexModule.java
@@ -1,5 +1,9 @@
 package com.widowcrawler.index.module;
 
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.google.inject.AbstractModule;
 import com.netflix.governator.annotations.Modules;
 import com.widowcrawler.core.module.WidowCoreModule;
@@ -14,6 +18,11 @@ public class WidowIndexModule extends AbstractModule {
 
     @Override
     public void configure() {
+        // TODO: The region needs to be config
+        AmazonDynamoDB dynamoDBClient = new AmazonDynamoDBClient();
+        dynamoDBClient.setRegion(Region.getRegion(Regions.US_WEST_2));
+
+        bind(AmazonDynamoDB.class).toInstance(dynamoDBClient);
         bind(WorkerProvider.class).to(IndexWorkerProvider.class).asEagerSingleton();
     }
 }
