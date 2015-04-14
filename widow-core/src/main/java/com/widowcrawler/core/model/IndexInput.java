72b36ea57d11df3e89365655169dfa3503eba983
Scott Mansfield <sgmansf@gmail.com>
2015-04-14 00:06:33 -0700

    IndexInput and ParseInput are now completely attribute-based. No individual fields.

--- widow-core/src/main/java/com/widowcrawler/core/model/IndexInput.java
+++ widow-core/src/main/java/com/widowcrawler/core/model/IndexInput.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,13 +12,6 @@ import java.util.Map;
  */
 public class IndexInput {
 
-    // Raw data from server
-    private String originalURL;
-    private Integer statusCode;
-    private Map<String, List<String>> headers;
-    private String pageContent;
-
-    // any extra data that can be collected
     private Map<PageAttribute, Object> attributes;
 
     public static class Builder {
@@ -33,26 +25,6 @@ public class IndexInput {
         public IndexInput build() {
             this.building.attributes = Collections.unmodifiableMap(this.building.attributes);
             return this.building;
-        }
-
-        public Builder withOriginalURL(String originalURL) {
-            this.building.originalURL = originalURL;
-            return this;
-        }
-
-        public Builder withStatusCode(Integer statusCode) {
-            this.building.statusCode = statusCode;
-            return this;
-        }
-
-        public Builder withHeaders(Map<String, List<String>> headers) {
-            this.building.headers = Collections.unmodifiableMap(headers);
-            return this;
-        }
-
-        public Builder withPageContent(String pageContent) {
-            this.building.pageContent = pageContent;
-            return this;
         }
 
         public Builder withExistingAttributes(Map<PageAttribute, Object> attributes) {
@@ -70,36 +42,15 @@ public class IndexInput {
     private IndexInput() { }
 
     @JsonCreator
-    private IndexInput(
-            @JsonProperty("originalURL")       String originalURL,
-            @JsonProperty("pageContent")       String pageContent,
-            @JsonProperty("headers")           Map<String, List<String>> headers,
-            @JsonProperty("status")            Integer statusCode,
-            @JsonProperty("attributes")        Map<PageAttribute, Object> attributes) {
-        this.originalURL = originalURL;
-        this.pageContent = pageContent;
-        this.headers = Collections.unmodifiableMap(headers);
-        this.statusCode = statusCode;
+    private IndexInput(@JsonProperty("attributes") Map<PageAttribute, Object> attributes) {
         this.attributes = Collections.unmodifiableMap(attributes);
-    }
-
-    public String getOriginalURL() {
-        return originalURL;
-    }
-
-    public String getPageContent() {
-        return pageContent;
-    }
-
-    public Map<String, List<String>> getHeaders() {
-        return headers;
-    }
-
-    public Integer getStatusCode() {
-        return statusCode;
     }
 
     public Map<PageAttribute, Object> getAttributes() {
         return attributes;
+    }
+
+    public Object getAttribute(PageAttribute pageAttribute) {
+        return this.attributes.get(pageAttribute);
     }
 }
--- widow-core/src/main/java/com/widowcrawler/core/model/ParseInput.java
+++ widow-core/src/main/java/com/widowcrawler/core/model/ParseInput.java
@@ -2,22 +2,16 @@ package com.widowcrawler.core.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.joda.time.DateTime;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Scott Mansfield
  */
 public class ParseInput {
 
-    // Raw data from server
-    private String originalURL;
-    private Integer statusCode;
-    private Map<String, List<String>> headers;
-    private String pageContent;
-
-    // any extra data that can be collected
     private Map<PageAttribute, Object> attributes;
 
     public static class Builder {
@@ -32,26 +26,6 @@ public class ParseInput {
             return this.building;
         }
 
-        public Builder withOriginalURL(String originalURL) {
-            this.building.originalURL = originalURL;
-            return this;
-        }
-
-        public Builder withStatusCode(Integer statusCode) {
-            this.building.statusCode = statusCode;
-            return this;
-        }
-
-        public Builder withHeaders(Map<String, List<String>> headers) {
-            this.building.headers = headers;
-            return this;
-        }
-
-        public Builder withPageContent(String pageContent) {
-            this.building.pageContent = pageContent;
-            return this;
-        }
-
         public Builder withAttribute(PageAttribute pageAttribute, Object value) {
             this.building.attributes.put(pageAttribute, value);
             return this;
@@ -61,38 +35,16 @@ public class ParseInput {
     // no-arg constructor for the builder
     private ParseInput() { }
 
-    // save me from my OCD
     @JsonCreator
-    private ParseInput(
-            @JsonProperty("originalURL")       String originalURL,
-            @JsonProperty("pageContent")       String pageContent,
-            @JsonProperty("headers")           Map<String, List<String>> headers,
-            @JsonProperty("status")            Integer statusCode,
-            @JsonProperty("attributes")        Map<PageAttribute, Object> attributes) {
-        this.originalURL = originalURL;
-        this.pageContent = pageContent;
-        this.headers = Collections.unmodifiableMap(headers);
-        this.statusCode = statusCode;
+    private ParseInput(@JsonProperty("attributes") Map<PageAttribute, Object> attributes) {
         this.attributes = Collections.unmodifiableMap(attributes);
-    }
-
-    public String getOriginalURL() {
-        return originalURL;
-    }
-
-    public String getPageContent() {
-        return pageContent;
-    }
-
-    public Map<String, List<String>> getHeaders() {
-        return headers;
-    }
-
-    public Integer getStatusCode() {
-        return statusCode;
     }
 
     public Map<PageAttribute, Object> getAttributes() {
         return attributes;
+    }
+
+    public Object getAttribute(PageAttribute pageAttribute) {
+        return this.attributes.get(pageAttribute);
     }
 }
