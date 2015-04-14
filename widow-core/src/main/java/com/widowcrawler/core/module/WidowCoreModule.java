516d118ca90647fbff7d2274ef09e9b041220128
Scott Mansfield <sgmansf@gmail.com>
2015-04-14 00:08:51 -0700

    Switching sqs to us-west and adding s3 and retry classes to core module

--- widow-core/src/main/java/com/widowcrawler/core/module/WidowCoreModule.java
+++ widow-core/src/main/java/com/widowcrawler/core/module/WidowCoreModule.java
@@ -1,9 +1,14 @@
 package com.widowcrawler.core.module;
 
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.google.inject.AbstractModule;
+import com.widowcrawler.core.retry.Retry;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -17,14 +22,21 @@ public class WidowCoreModule extends AbstractModule {
     @Override
     protected void configure() {
 
-        // Singletons
-        bind(AmazonSQSAsyncClient.class).asEagerSingleton();
+        // TODO: Make region config
+        AmazonSQSAsyncClient amazonSQSAsyncClient = new AmazonSQSAsyncClient();
+        amazonSQSAsyncClient.setRegion(Region.getRegion(Regions.US_WEST_2));
+        bind(AmazonSQSAsyncClient.class).toInstance(amazonSQSAsyncClient);
 
-        // Specific instances
+        AmazonS3 amazonS3Client = new AmazonS3Client();
+        amazonS3Client.setRegion(Region.getRegion(Regions.US_WEST_2));
+        bind(AmazonS3.class).toInstance(amazonS3Client);
+
         bind(ExecutorService.class).toInstance(Executors.newCachedThreadPool());
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JodaModule());
         bind(ObjectMapper.class).toInstance(mapper);
+
+        bind(Retry.class).asEagerSingleton();
     }
 }
