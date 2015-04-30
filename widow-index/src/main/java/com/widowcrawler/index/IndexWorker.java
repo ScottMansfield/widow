9067ca27a796817fa4f34cf892c4df251ef363e5
Scott Mansfield <sgmansf@gmail.com>
2015-04-30 00:17:53 -0700

    Adding ability to write to a MySQL db as well as DynamoDB

--- widow-index/src/main/java/com/widowcrawler/index/IndexWorker.java
+++ widow-index/src/main/java/com/widowcrawler/index/IndexWorker.java
@@ -8,12 +8,20 @@ import com.amazonaws.services.dynamodbv2.model.ReturnConsumedCapacity;
 import com.netflix.archaius.Config;
 import com.widowcrawler.core.model.IndexInput;
 import com.widowcrawler.core.model.PageAttribute;
+import com.widowcrawler.core.retry.RetryFailedException;
 import com.widowcrawler.core.worker.Worker;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -30,6 +38,9 @@ public class IndexWorker extends Worker {
 
     @Inject
     AmazonDynamoDB dynamoDBClient;
+
+    @Inject
+    DataSource dataSource;
 
     private IndexInput indexInput;
 
@@ -49,34 +60,8 @@ public class IndexWorker extends Worker {
                 throw new IllegalArgumentException("Attributes ORIGINAL_URL and TIME_ACCESSED must exist");
             }
 
-            Map<String, AttributeValue> attributeValueMap = indexInput.getAttributes().entrySet()
-                    .stream()
-                    .filter((entry) -> entry.getValue() != null)
-                    .filter((entry) -> StringUtils.isNotBlank(entry.getValue().toString()))
-                    .collect(Collectors.toMap(
-                            e -> e.getKey().toString(),
-                            e -> {
-                                switch (e.getKey().getType()) {
-                                    case NUMBER:
-                                        return new AttributeValue().withN(e.getValue().toString());
-
-                                    case STRING:
-                                    default:
-                                        return new AttributeValue().withS(e.getValue().toString());
-                                }
-                            }
-                    ));
-
-            String tableName = config.getString(TABLE_NAME_CONFIG_KEY);
-
-            PutItemRequest putItemRequest = new PutItemRequest()
-                    .withTableName(tableName)
-                    .withItem(attributeValueMap)
-                    .withReturnConsumedCapacity(ReturnConsumedCapacity.TOTAL);
-
-            final PutItemResult putItemResult = retry(() -> dynamoDBClient.putItem(putItemRequest));
-
-            logger.info("Consumed table capacity: " + putItemResult.getConsumedCapacity().getCapacityUnits());
+            writeToDynamo();
+            //writeToRDBMS();
 
             return true;
 
@@ -84,5 +69,77 @@ public class IndexWorker extends Worker {
             logger.error("Error while indexing.", ex);
             return false;
         }
+    }
+
+    private void writeToDynamo() throws RetryFailedException, InterruptedException {
+        Map<String, AttributeValue> attributeValueMap = indexInput.getAttributes().entrySet()
+                .stream()
+                .filter((entry) -> entry.getValue() != null)
+                .filter((entry) -> StringUtils.isNotBlank(entry.getValue().toString()))
+                .collect(Collectors.toMap(
+                        e -> e.getKey().toString(),
+                        e -> {
+                            switch (e.getKey().getType()) {
+                                case NUMBER:
+                                    return new AttributeValue().withN(e.getValue().toString());
+
+                                case STRING:
+                                default:
+                                    return new AttributeValue().withS(e.getValue().toString());
+                            }
+                        }
+                ));
+
+        String tableName = config.getString(TABLE_NAME_CONFIG_KEY);
+
+        PutItemRequest putItemRequest = new PutItemRequest()
+                .withTableName(tableName)
+                .withItem(attributeValueMap)
+                .withReturnConsumedCapacity(ReturnConsumedCapacity.TOTAL);
+
+        final PutItemResult putItemResult = retry(() -> dynamoDBClient.putItem(putItemRequest));
+
+        logger.info("Consumed table capacity: " + putItemResult.getConsumedCapacity().getCapacityUnits());
+    }
+
+    private void writeToRDBMS() throws SQLException, NoSuchAlgorithmException {
+        Connection connection = dataSource.getConnection();
+        connection.prepareStatement("use widow;").execute();
+
+        final PreparedStatement statement = connection.prepareStatement(
+                "insert into " +
+                "page_data(time_accessed," +
+                        "  original_url_hash," +
+                        "  original_url," +
+                        "  load_time_millis," +
+                        "  status_code," +
+                        "  headers," +
+                        "  response_size," +
+                        "  content_size," +
+                        "  page_content_ref," +
+                        "  title," +
+                        "  out_links," +
+                        "  css_links," +
+                        "  img_links," +
+                        "  js_links," +
+                        "  size_with_assets)" +
+                "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        statement.setLong(  1 , Long.valueOf(indexInput.getAttribute(PageAttribute.TIME_ACCESSED).toString()));
+        statement.setString(2 , DigestUtils.shaHex(indexInput.getAttribute(PageAttribute.ORIGINAL_URL).toString()));
+        statement.setString(3 , indexInput.getAttribute(PageAttribute.ORIGINAL_URL).toString());
+        statement.setLong(  4 , Double.valueOf(indexInput.getAttribute(PageAttribute.LOAD_TIME_MILLIS).toString()).longValue());
+        statement.setInt(   5 , Integer.valueOf(indexInput.getAttribute(PageAttribute.STATUS_CODE).toString()));
+        statement.setString(6 , indexInput.getAttribute(PageAttribute.HEADERS).toString());
+        statement.setInt(   7 , Integer.valueOf(indexInput.getAttribute(PageAttribute.RESPONSE_SIZE).toString()));
+        statement.setInt(   8 , Integer.valueOf(indexInput.getAttribute(PageAttribute.CONTENT_SIZE).toString()));
+        statement.setString(9 , indexInput.getAttribute(PageAttribute.PAGE_CONTENT_REF).toString());
+        statement.setString(10, indexInput.getAttribute(PageAttribute.TITLE).toString());
+        statement.setString(11, indexInput.getAttribute(PageAttribute.OUT_LINKS).toString());
+        statement.setString(12, indexInput.getAttribute(PageAttribute.CSS_LINKS).toString());
+        statement.setString(13, indexInput.getAttribute(PageAttribute.IMG_LINKS).toString());
+        statement.setString(14, indexInput.getAttribute(PageAttribute.JS_LINKS).toString());
+        statement.setInt(   15, Integer.valueOf(indexInput.getAttribute(PageAttribute.SIZE_WITH_ASSETS).toString()));
+
+        statement.executeUpdate();
     }
 }
--- widow-index/src/main/java/com/widowcrawler/index/module/WidowIndexModule.java
+++ widow-index/src/main/java/com/widowcrawler/index/module/WidowIndexModule.java
@@ -1,11 +1,18 @@
 package com.widowcrawler.index.module;
 
 import com.google.inject.AbstractModule;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSourceFactory;
 import com.netflix.governator.annotations.Modules;
 import com.widowcrawler.core.module.DynamoDBModule;
 import com.widowcrawler.core.module.WidowCoreModule;
 import com.widowcrawler.core.worker.WorkerProvider;
 import com.widowcrawler.index.IndexWorkerProvider;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * @author Scott Mansfield
@@ -14,6 +21,16 @@ import com.widowcrawler.index.IndexWorkerProvider;
 public class WidowIndexModule extends AbstractModule {
     @Override
     public void configure() {
+        // TODO: Make this all config
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl("jdbc:mysql://widow-test.c0endjsljhny.us-west-2.rds.amazonaws.com");
+        dataSource.setPort(3306);
+        dataSource.setDatabaseName("widow");
+        dataSource.setUser("masterwidow");
+        dataSource.setPassword("thisismypassword");
+
+        bind(DataSource.class).toInstance(dataSource);
+
         bind(WorkerProvider.class).to(IndexWorkerProvider.class).asEagerSingleton();
     }
 }
